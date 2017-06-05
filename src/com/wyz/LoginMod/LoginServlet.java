cc22f364d7f293b8820bdbc0d9fbe8e6e38e9168
JHQ <cc_jhq@163.com>
2017-06-05 22:31:26 +0800

    更改

--- src/com/wyz/LoginMod/LoginServlet.java
+++ src/com/wyz/LoginMod/LoginServlet.java
@@ -17,8 +17,7 @@ public class LoginServlet extends HttpServlet{
 	 * @author 姜华起  @time 2016.2.29
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	@Override
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String phone = request.getParameter("phone");
@@ -33,8 +32,8 @@ public class LoginServlet extends HttpServlet{
 		request.setAttribute("login", loginResponse);
 		request.getRequestDispatcher("/web-app/login.jsp").forward(request, response);
 	}
-	
-	@Override
+
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 doGet(request, response);
    }
